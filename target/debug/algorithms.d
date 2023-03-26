230d410d0f47da8c4ffe77a0458906f05fa1d9da
Chris Muga <chrismuga94@gmail.com>
2023-03-26 19:20:24 +0300

    remove .gitignore files

--- target/debug/algorithms.d
+++
@@ -1,1 +0,0 @@
-/Users/muga/lab/rust/algorithms/target/debug/algorithms: /Users/muga/lab/rust/algorithms/src/integer_to_roman.rs /Users/muga/lab/rust/algorithms/src/main.rs
--- target/debug/deps/algorithms-11ed80eb87e6020a.d
+++
@@ -1,6 +0,0 @@
-/Users/muga/lab/rust/algorithms/target/debug/deps/algorithms-11ed80eb87e6020a: src/main.rs src/integer_to_roman.rs
-
-/Users/muga/lab/rust/algorithms/target/debug/deps/algorithms-11ed80eb87e6020a.d: src/main.rs src/integer_to_roman.rs
-
-src/main.rs:
-src/integer_to_roman.rs:
